43ff6f7b03ae36a228e23db03fe45e491d7f5588
kujanenj <jussi.kujanen@gmail.com>
2021-07-24 21:17:16 +0300

    fix eslint problems

--- src/App.js
+++ src/App.js
@@ -27,24 +27,10 @@ const App = () => {
     }
   ])
 
-  const [notification, setNotification] = useState('')
 
  const addNew = (anecdote) => {
     anecdote.id = (Math.random() * 10000).toFixed(0)
    setAnecdotes(anecdotes.concat(anecdote))
-  }
-
- const anecdoteById = (id) =>
-   anecdotes.find(a => a.id === id)
-
-  const vote = (id) => {
-    const anecdote = anecdoteById(id)
-
-    const voted = {
-     ...anecdote,
-     votes: anecdote.votes + 1    }
-
-  setAnecdotes(anecdotes.map(a => a.id === id ? voted : a))
   }
 
   
